71cf810de9000ce95bfdb754b33d101345351ab0
Deep Sohelia <soheliadeep@gmail.com>
2015-10-03 21:56:06 +0100

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -1,4 +1,4 @@
-var posts = ["helloWorld"]
+var posts = ["hello-world"]
 
 function ahah(url) {
 	var targetElement = document.body;
@@ -12,31 +12,50 @@ function ahah(url) {
 			console.log = "ahah error:\n" + request.statusText;
 			return;
 		}
-		targetElement.innerHTML += request.responseText;
+		//targetElement.innerHTML += request.responseText;
+		document.getElementById(request.responseText.split("<article id=\"")[1].split("\">")[0]).innerHTML=request.responseText
+		
 	}
 	request.open("GET", url, true);
 	request.send();
 }
-
 var numOfPosts;
 function loadPosts() {
 	numOfPosts = posts.length;
 	while(numOfPosts--) {
+		document.body.innerHTML+="<div id=\"" + posts[numOfPosts] + "\"></div>"
 		ahah("posts/" + posts[numOfPosts] + ".html");
+	}
+}
+var url;
+function checkURL() {
+	url = window.location.href.split("?")[1]
+	if ((url != undefined) && (posts.indexOf(url) != -1)) {
+		console.log(url);
+		document.querySelector("article#" + url).className = "fs";
+		document.querySelector("article#" + url + " a.readMore").innerText = "Go Back";
+		console.log("Expanded Article")
 	}
 }
 
 window.onload = function() {
     document.addEventListener('click', function(evt) {
+        console.log(evt.target.parentNode);
+        console.log(evt.target.innerText);
+        console.log(evt.target.tagName)
+        console.log(evt.target.parentNode.id)
         if((evt.target.tagName == "A") && (evt.target.innerText == "Read more")) {
             evt.preventDefault();
             evt.target.parentNode.className = "fs";
             evt.target.innerText = "Go back"
+            history.pushState({}, evt.target.parentNode.id + ".html", window.location.href + "/?" + evt.target.parentNode.id);
         } else if((evt.target.tagName == "A") && (evt.target.innerText == "Go back")) {
+            history.pushState({}, "Blog", "http://picoknow.github.io/amicum/blog");
             evt.preventDefault();
             evt.target.parentNode.className = "";
             evt.target.innerText = "Read more";
         }
     }, false);
-    loadPosts()
+    loadPosts();
+    setTimeout(checkURL,500);
 }
